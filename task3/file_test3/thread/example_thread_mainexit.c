3e4d0737b6280ea74c97c5933a85109d260a12e9
DinhThien <toidaykt357@gmail.com>
2024-08-16 23:04:06 +0700

    Update thread folder in file_test3

---
+++ task3/file_test3/thread/example_thread_mainexit.c
@@ -0,0 +1,38 @@
+#include <pthread.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/syscall.h>
+
+#define NUM_THREADS 2
+
+void *thread_print(void* threadid) {
+  long tid;
+  tid = (long)threadid;
+  printf("Hello bro: I'm thread #%ld! My thread's ID: %ld\n", tid, syscall(SYS_gettid));
+  sleep(100);
+}
+
+int main() {
+  pthread_t threads[NUM_THREADS];
+  int check;
+  long tID;
+
+  for(tID = 0; tID < NUM_THREADS; tID++) {
+    printf("I'm main thread: create thread: #%ld\n", tID);
+    check = pthread_create(
+        &threads[tID],
+        NULL,
+        thread_print,
+        (void*)tID
+        );
+    if(check != 0) {
+      printf("ERROR!! I'm main thread, can't create thread #%ld\n", tID);
+      exit(-1);
+    }
+  }
+  sleep(100);
+  /* Last thing main should do*/
+  pthread_exit(NULL);
+  return 0;
+}
